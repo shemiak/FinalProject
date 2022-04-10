96e72748da07255bd56fe4e81a2501b701e2c838
shemiak <67714094+shemiak@users.noreply.github.com>
2022-04-11 02:12:08 +0300

    FPROJ-7 Реализован запрос баланса на стороне клиента через RestTemplate (#6)

---
+++ client/src/main/java/org/finalProject/client/dto/BalanceDto.java
@@ -0,0 +1,12 @@
+package org.finalProject.client.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class BalanceDto {
+    private BigDecimal balance;
+}
